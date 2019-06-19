73cfefa44b52c65a27bf466e2b8c37ba1d8af46a
cartalink <ngommouhamedlamine95@gmail.com>
2019-06-19 14:39:55 +0000

    first init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { ScrollToModule } from '@nicky-lenaers/ngx-scroll-to';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './shared/nav-bar/nav-bar.component';
+import { MainComponent } from './components/main/main.component';
+import { ActivitiesComponent } from './components/activities/activities.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ServiceComponent } from './components/service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MainComponent,
+    ActivitiesComponent,
+    AboutusComponent,
+    HomeComponent,
+    ContactComponent,
+    FooterComponent,
+    ServiceComponent,
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    ScrollToModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
